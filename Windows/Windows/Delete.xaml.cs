b0f040559c53c90b2887cc895782ba50aad18826
Kvasquez2 <66072723+Kvasquez2@users.noreply.github.com>
2023-09-19 20:15:52 -0500

    Ventanas

---
+++ Windows/Windows/Delete.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Windows
+{
+    /// <summary>
+    /// Lógica de interacción para Delete.xaml
+    /// </summary>
+    public partial class Delete : Window
+    {
+        Consultas mconsulta;
+        public Delete()
+        {
+            InitializeComponent();
+            mconsulta = new Consultas();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            int datoID = int.Parse(campoDelete.Text);
+            mconsulta.Deletes(datoID);
+            MessageBox.Show("Datos Eliminados");
+        }
+    }
+}
---
+++ Windows/Windows/Update.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Windows
+{
+    /// <summary>
+    /// Lógica de interacción para Update.xaml
+    /// </summary>
+    public partial class Update : Window
+    {
+        Consultas mconsulta;
+        public Update()
+        {
+            InitializeComponent();
+            mconsulta = new Consultas();
+        }
+
+        private void btnUpdates_Click(object sender, RoutedEventArgs e)
+        {
+            int datoPrecio = int.Parse(campoPrecio.Text);
+            int datoStock = int.Parse(campoStock.Text);
+            int datoIdInventario = int.Parse(campoIdInvent.Text);
+            mconsulta.updates(datoIdInventario, datoPrecio, datoStock, campoProveedor.Text);
+            MessageBox.Show("Los datos se han actualizado correctamente");
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
